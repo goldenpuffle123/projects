a54dcc1a266eb15643b7534614cb515f474eb9ce
goldenpuffle123 <82688745+goldenpuffle123@users.noreply.github.com>
2025-12-29 16:57:40 -0500

    Upload javascript_exercises from Odin, finish 1-5
    - https://github.com/TheOdinProject/javascript-exercises

---
+++ js_exercises_top/foundations/03_numberChecker/numberChecker.js
@@ -0,0 +1,11 @@
+function numberChecker(number) {
+  return (number >= 10);
+  // if (number >= 10) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
