67eb60073464bd64c4057c44af182a1f32b56f81
goldenpuffle123 <82688745+goldenpuffle123@users.noreply.github.com>
2025-12-30 16:14:21 -0500

    Finish 6-11

--- js_exercises_top/foundations/08_removeFromArray/removeFromArray.js
+++ js_exercises_top/foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    return arr.filter(
+        (elem) => !(nums.includes(elem))
+    );
 };
 
 // Do not edit below this line
--- js_exercises_top/foundations/11_tempConversion/tempConversion.js
+++ js_exercises_top/foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg_f) {
+  res = (deg_f-32)*(5/9)
+  return Math.round(res*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg_c) {
+  res = deg_c*9/5+32
+  return Math.round(res*10)/10;
 };
 
 // Do not edit below this line
