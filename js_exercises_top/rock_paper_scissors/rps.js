6fd9b999caf04efa494a4e8834afae79015c896a
goldenpuffle123 <82688745+goldenpuffle123@users.noreply.github.com>
2025-12-31 00:41:49 -0500

    Finish rps with UI

--- js_exercises_top/rock_paper_scissors/rps.js
+++ js_exercises_top/rock_paper_scissors/rps.js
@@ -10,12 +10,10 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Enter choice (rock, paper, scissors): ").toLowerCase();
-    return humanChoice;
-}
-
-function playRound(computerChoice, humanChoice) {
+function playRound() {
+    // Encoding: computer: 0, user: 1, tie: 2
+    computerChoice = getComputerChoice();
+    console.log(computerChoice, humanChoice)
     let winner;
     if(computerChoice=="rock" && humanChoice=="paper") winner = 1;
     else if(computerChoice=="paper" && humanChoice=="rock") winner = 0;
@@ -26,23 +24,52 @@ function playRound(computerChoice, humanChoice) {
     else if(computerChoice == humanChoice) winner = 2;
     else return alert("Error");
 
+    updateWin(winner);
+
+}
+
+function updateWin(winner) {
+    const winnerText = document.querySelector("[data-winner]");
+    const userScoreText = document.querySelector("[data-user-score]");
+    const computerScoreText = document.querySelector("[data-computer-score]");
     if(winner == 0) {
-        console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
+        winnerText.textContent = "COMPUTER"
         computerScore++;
     }
     else if(winner == 1) {
-        console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+        winnerText.textContent = "USER"
         humanScore++;
     }
     else {
-        console.log(`Tie, ${computerChoice} and ${humanChoice}`);
+        winnerText.textContent = "TIE"
     }
+    userScoreText.textContent = humanScore;
+    computerScoreText.textContent = computerScore;
+    nextButton.disabled = false;
+    
 
 }
+
+const rpsButtons = document.querySelector("#buttons");
+const nextButton = document.querySelector("#next");
 
 let humanScore = 0;
 let computerScore = 0;
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-playRound(computerSelection, humanSelection);
+nextButton.disabled = true;
+let computerChoice;
+let humanChoice;
+
+rpsButtons.addEventListener("click", (event) => {
+    target = event.target;
+    humanChoice=target.id;
+    target.style["border-color"] = "hsl(300, 100%, 35%)";
+    if (nextButton.disabled) playRound();
+});
+
+nextButton.addEventListener( "click",
+    () => {
+        document.querySelectorAll("#buttons>button").forEach((btn) => btn.style["border-color"] = "gray"); // remove effect
+        nextButton.disabled = true;
+    }
+)
